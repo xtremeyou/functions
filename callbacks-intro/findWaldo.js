605a1f9fd4f68cca5bea339416a2eb0affd79d66
Merak Bourque <xtremeyou@hotmail.com>
2024-06-28 00:02:36 -0400

    Updated version with anonymous callback

--- callbacks-intro/findWaldo.js
+++ callbacks-intro/findWaldo.js
@@ -5,8 +5,11 @@ const findWaldo = function (names, found) {
     }
   });
 }; 
-const actionWhenFound = function (index) {
-  console.log("Found him at index:", index);
-};
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function (index) {
+  console.log("Waldo is located at:", index);
+});
+
+//const actionWhenFound = function (index) {
+  //console.log("Found him at index:", index);
+//};
